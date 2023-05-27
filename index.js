02db2949e9454c3c0c24f579346491e23643bcfd
Oscar Hoodsmith <HoodsmithO@cardiff.ac.uk>
2023-05-27 11:36:40 +0100

    Added existing joun code

---
+++ index.js
@@ -0,0 +1,56 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client
+    ({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.GuildMembers,
+        ],
+    });
+
+// Users
+const oscar = '256685341723983872';
+const jack = '641702978910158849';
+
+// Test Server
+const cope_channel_test = '1111364069329485916';
+
+// Big Big Chungus
+const cope_channel = '1109161279089807370'
+
+// Load msg
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    });
+
+
+// Message From User
+client.on('messageCreate', async (message) =>
+{
+    if (message.author.id == jack) {
+        let date = new Date().toLocaleString();
+        const cope = client.channels.cache.get(cope_channel);
+        cope.send(' ' + date + ':\n `"' + message.content + '"`');
+    }
+});
+
+// Reply Messages
+client.on('messageCreate', async (message) =>
+{
+    let msg = message.content.toLowerCase();
+
+    switch (msg)
+    {
+        case 'jack':
+            message.reply('is balding');
+            break;
+        case 'stick':
+            message.reply({ files: ['assets/stick.png'] });
+    }
+});
+        
+client.login(process.env.CLIENT_TOKEN);
