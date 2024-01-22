6ab028ffb6e30daaa98a5ee2ccd67510df8654d4
Oscar Hoodsmith <55330213+JellyNutJim@users.noreply.github.com>
2024-01-22 16:09:05 +0000

    small update

--- commands/Utility/ping.js
+++ commands/Utility/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Responds with latency on reply. (Not API latency)'),
+		.setDescription('Responds with latency.'),
 	async execute(interaction) {
 		await interaction.reply('Latency is: ' + (Math.abs(interaction.createdTimestamp - Date.now())) + 'ms.');
 	},
